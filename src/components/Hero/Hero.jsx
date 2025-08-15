e7795c4e578e42651fba702811a1fd117bc793c7
Yulong-Dev <onyiigodset@gmail.com>
2025-08-15 08:52:54 +0100

    am back

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -11,7 +11,7 @@ const Hero = () => {
             <p>I am a fullstack developer from Imo state, Nigeria, with 5years of experience in multiple companies like JengaTech, Billpoint and Fairmoney. </p>
             <div className="hero-action">
             <a href="https://linkedin.com/in/gowinstar55555"><div className="hero-connect"> Connect with me</div></a>
-                <div className="hero-resume">My resume</div>
+                <a className="hero-resume" href="https://drive.google.com/file/d/1bewnBUHNyU7oM9p1IbcrCmw4tWBEGA9K/view?usp=drivesdk" >My resume</a>
             </div>
         </div>
       
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,50 +1,80 @@
-import React, { useRef, useState } from 'react';
-import './Navbar.css';
-import theme_pattern from '../../assets/theme_pattern.svg';
-import underline from '../../assets/nav_underline.svg';
+import React, { useRef, useState } from "react";
+import "./Navbar.css";
+import theme_pattern from "../../assets/theme_pattern.svg";
+import underline from "../../assets/nav_underline.svg";
 import { RiMenu3Line } from "react-icons/ri";
 import { IoClose } from "react-icons/io5";
-import { Link } from 'react-router-dom';
-import Toggle from '../Toggle';
-
-
+import { Link, useLocation } from "react-router-dom";
+import { motion } from "framer-motion";
+import Toggle from "../Toggle";
 
 const Navbar = () => {
-    const [menu,setMenu] = useState("home");
-    const menuRef = useRef();
+  const [menu, setMenu] = useState("home");
+  const menuRef = useRef();
+  const location = useLocation();
 
-    const openMenu = () => {
-        menuRef.current.style.right="0px";
-    };
-    const closeMenu = () => {
-        menuRef.current.style.right="-350px";
-    };
+  const links = [
+    { to: "/", label: "Home", key: "home" },
+    { to: "/about", label: "About Me", key: "about" },
+    { to: "/services", label: "Services", key: "services" },
+    { to: "/port", label: "Portfolio", key: "work" },
+    { to: "/contact", label: "Contact", key: "contact" },
+  ];
 
- 
+  const openMenu = () => {
+    menuRef.current.style.right = "0px";
+  };
+  const closeMenu = () => {
+    menuRef.current.style.right = "-350px";
+  };
 
-    
-    
-    return (
-        <div className='nav'>
-            <div className="first-nav"><Link to='/'>
-               <h1>George<span>.</span></h1>
-               <img src={theme_pattern} alt="" /></Link>
-            </div>
-            <RiMenu3Line onClick={openMenu} className='nav-mob-open'/>
-            <ul ref={menuRef} className="nav-menu">
-                <IoClose  alt="" onClick={closeMenu} className='nav-mob-close'/>
-                <li><Link to='/' className='link'><p onClick={()=>setMenu("home")}>Home</p>{menu==="home"?<img src={underline} alt=''/>:<></>}</Link></li>
-                <li><Link to='/about' className='link'><p onClick={()=>setMenu("about")}>About Me</p>{menu==="about"?<img src={underline} alt=''/>:<></>}</Link></li>
-                <li><Link to='/services' className='link'><p onClick={()=>setMenu("services")}>Services</p>{menu==="services"?<img src={underline} alt=''/>:<></>}</Link></li>
-                <li><Link to='/port' className='link'><p onClick={()=>setMenu("work")}>Portfolio</p>{menu==="work"?<img src={underline} alt=''/>:<></>}</Link></li>
-                <li><Link to='/contact' className='link'><p onClick={()=>setMenu("contact")}>Contact</p>{menu==="contact"?<img src={underline} alt=''/>:<></>}</Link></li>
-            </ul>
-            <div className='cup'>
-                <Toggle/>
-            <a href="https://linkedin.com/in/gowinstar55555" className="nav-connect"><div><i>Connect</i></div></a>
-            </div> 
-        </div>
-    )
+  return (
+    <div className="nav">
+      <div className="first-nav">
+        <Link to="/">
+          <h1>
+            George<span>.</span>
+          </h1>
+          <img src={theme_pattern} alt="" />
+        </Link>
+      </div>
+      <RiMenu3Line onClick={openMenu} className="nav-mob-open" />
+      <ul ref={menuRef} className="nav-menu">
+        <IoClose onClick={closeMenu} className="nav-mob-close" />
+        {links.map((link) => {
+          const isActive = location.pathname === link.to;
+
+          return (
+            <li key={link.key} className="relative">
+              <Link to={link.to} className="link z-10 relative">
+                {link.label}
+                {isActive && (
+                  <motion.img
+                    src={underline}
+                    alt="underline"
+                    className="absolute -right-2 -z-2 bottom-0"
+                    layoutId="nav-underline"
+                    transition={{ type: "spring", stiffness: 400, damping: 25 }}
+                  />
+                )}
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+      <div className="cup">
+        <Toggle />
+        <a
+          href="https://linkedin.com/in/gowinstar55555?"
+          className="nav-connect"
+        >
+          <div>
+            <i>Connect</i>
+          </div>
+        </a>
+      </div>
+    </div>
+  );
 };
 
-export default Navbar
+export default Navbar;
